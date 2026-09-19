9552d6191e22b6d2f3d09270882b9e9653fff2fe
Michael Klein <michaelklein1997@gmail.com>
2026-09-19 17:10:11 -0500

    Added backend and gitignores

---
+++ backend/src/test/java/com/kleintwins/tcg_tracker/TcgTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleintwins.tcg_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
